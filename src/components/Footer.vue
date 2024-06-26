560e858c072ad337f3183c4ac7e4cdb9b51d08ef
AChar1f <acharif41@gmail.com>
2024-06-26 11:44:12 +0200

    footer setup

---
+++ src/components/Footer.vue
@@ -0,0 +1,28 @@
+<template>
+    <footer class="container-fluid">
+        <div class="row">
+            <p class="lead">Abdurahamaan's Portfolio
+                <span>&copy;{{ cYear }}</span>
+            </p>
+        </div>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'FooterComp',
+    data() {
+        return {
+            cYear : new Date().getUTCFullYear()
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .container-fluid {
+        color: whitesmoke;
+        border-top: 4px solid #A29D95;
+        background-image: linear-gradient(180deg, black 50%,#232323)
+    }
+</style>
