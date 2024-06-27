42a0990657f1d80e3514cd64159792a3296a16e2
AChar1f <acharif41@gmail.com>
2024-06-27 11:26:46 +0200

    landing page data

--- src/components/LandingSection.vue
+++ src/components/LandingSection.vue
@@ -1,38 +1,65 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col">
-                <img src="https://achar1f.github.io/projectimages/images/me.png" alt="profile" loading="lazy" class="img-fluid">
-            </div>
-            <div class="col">
-                <div id="details">
-                    <h1 class="display-1">Abdurahmaan Charif</h1>
-                    <p v-if="jobTitle?.length"><span>{{ jobTitle[2]?.role }}</span></p>
-                    <div v-else class="d-flex justify-content-center">
-                    <div class="spinner-border" role="status">
-                    </div>
-</div>
-                </div>
-            </div>
+  <div class="container">
+    <div class="row vh-100 align-items-center">
+      <div class="col">
+        <img
+          src="https://achar1f.github.io/projectimages/images/me.png"
+          alt="profile"
+          loading="lazy"
+          class="img-fluid rounded-2"
+        />
+      </div>
+      <div class="col">
+        <div id="details">
+          <h2 class="display-2">Abdurahmaan Charif</h2>
+          <p v-if="title">
+            <span>The {{ title }}</span>
+          </p>
+          <Spinner v-else/>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script setup>
-import {computed, onMounted} from 'vue'
-import { useStore } from 'vuex'
+import Spinner from './Spinner.vue'
+import { computed, onMounted, ref } from "vue";
+import { useStore } from "vuex";
+import Swal from "sweetalert2/dist/sweetalert2";
 
-const store = useStore()
-const jobTitle = computed(() => store.state.jobTitle)
+const store = useStore();
+const jobTitle = computed(() => store.state.jobTitle);
+const title = ref("Junior Developer");
+const cnt = ref(-1);
+
+function repeat() {
+  try {
+    if (cnt.value == jobTitle.value?.length) cnt.value = 0;
+    title.value = jobTitle.value?.at(cnt.value)?.role;
+    setTimeout(repeat, 2000);
+    cnt.value++;
+  } catch (e) {
+    //insert sweet alert
+    Swal.fire({
+      title: "Error",
+      text: "Failed to fetch about data",
+      icon: "error",
+      timer: 2000,
+    });
+  }
+}
+
 onMounted(() => {
-    store.dispatch('fetchJobTitle')
-})
+  store.dispatch("fetchJobTitle");
+  repeat();
+});
+//ref allows you to make use of a primitive data type in comp ai
+//reactive allows you to make use of mutative data types in comp ai
 // export default {  // used in options currently composition is running
 //     name: 'LandingSection'
 // }
 </script>
 
 <style scoped>
-    
 </style>
-
